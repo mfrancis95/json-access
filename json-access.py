3a8a639a53ff08047f66e357f6efc1bbbb021fa1
mfrancis95 <mikefrancis95@gmail.com>
2016-01-14 19:35:58 -0500

    Passing no arguments will print out the whole JSON.

--- json-access.py
+++ json-access.py
@@ -1,18 +1,22 @@
 from argparse import ArgumentParser
-from json import load
+import json
 import sys
 
 parser = ArgumentParser("json-access")
-parser.add_argument("keys", nargs = "+")
+parser.add_argument("keys", nargs = "*")
 args = parser.parse_args()
 
-json = load(sys.stdin)
+keys = args.keys
+data = json.load(sys.stdin)
 
-for key in map(lambda key: key.split("."), args.keys):
-    try:
-        value = json[key[0]]
-        for subkey in key[1:]:
-            value = value[subkey]
-        print(value)
-    except KeyError:
-        pass
+if keys:
+    for key in map(lambda key: key.split("."), args.keys):
+        try:
+            value = data[key[0]]
+            for subkey in key[1:]:
+                value = value[subkey]
+            print(value)
+        except KeyError:
+            pass
+else:
+    print(data)
