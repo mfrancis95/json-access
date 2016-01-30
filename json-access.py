5451c93add62a0498ad238ef4e812cc25d228cd5
mfrancis95 <mikefrancis95@gmail.com>
2016-01-29 19:06:50 -0500

    Add --file argument for reading from files instead of stdin

--- json-access.py
+++ json-access.py
@@ -4,17 +4,24 @@ import sys
 from functools import reduce
 
 parser = ArgumentParser("json-access")
+parser.add_argument("-f", "--file")
 parser.add_argument("keys", nargs = "*")
 args = parser.parse_args()
 
-keys = args.keys
+def print_json(file, keys):
+    for json in load(file):
+        if keys:
+            for key in map(lambda key: key.split("."), keys):
+                try:
+                    print(reduce(lambda value, subkey: value[subkey], key, json))
+                except KeyError:
+                    pass
+        else:
+            print(json)
 
-for data in load(sys.stdin):
-    if keys:
-        for key in map(lambda key: key.split("."), keys):
-            try:
-                print(reduce(lambda value, subkey: value[subkey], key, data))
-            except KeyError:
-                pass
-    else:
-        print(data)
+file = args.file
+if file:
+    with open(file) as f:
+        print_json(f, args.keys)
+else:
+    print_json(sys.stdin, args.keys)
