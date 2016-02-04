98bee15400b0d3e37a13bdccf552500ea7d03b82
mfrancis95 <mikefrancis95@gmail.com>
2016-02-03 21:04:56 -0500

    Add --indent argument for pretty printing

--- json-access.py
+++ json-access.py
@@ -1,23 +1,27 @@
 from argparse import ArgumentParser
 from lazyjson import load
+from json import dumps
 import sys
 from functools import reduce
 
 parser = ArgumentParser("json-access")
 parser.add_argument("-f", "--file")
+parser.add_argument("-i", "--indent", default = 4, type = int)
 parser.add_argument("keys", nargs = "*")
 args = parser.parse_args()
+
+indent = args.indent
 
 def print_json(file, keys):
     for json in load(file):
         if keys:
             for key in map(lambda key: key.split("."), keys):
                 try:
-                    print(reduce(lambda value, subkey: value[subkey], key, json))
+                    print(dumps(reduce(lambda value, subkey: value[subkey], key, json), indent = indent))
                 except KeyError:
                     pass
         else:
-            print(json)
+            print(dumps(json, indent = indent))
 
 file = args.file
 if file:
