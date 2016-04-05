cfbdad1e1f12258ea4418d4d48e0e3d24884e3c6
mfrancis95 <mikefrancis95@gmail.com>
2016-04-05 09:23:16 -0400

    Add put subcommand

--- quick-json.py
+++ quick-json.py
@@ -12,40 +12,68 @@ subparsers = parser.add_subparsers(dest = "command")
 getparser = subparsers.add_parser("get")
 getparser.add_argument("keys", nargs = "*")
 
+putparser = subparsers.add_parser("put")
+putparser.add_argument("key")
+putparser.add_argument("value")
+
 removeparser = subparsers.add_parser("remove")
 removeparser.add_argument("keys", nargs = "+")
 
 args = parser.parse_args()
 indent = args.indent
-keys = list(map(lambda key: key.split("."), args.keys))
+command = args.command
 
 def json_get(file):
+    keys = list(map(lambda key: key.split("."), args.keys))
     for json in load(file):
         if keys:
-            for key in keys:
+            for subkeys in keys:
                 try:
-                    print(dumps(reduce(lambda value, subkey: value[subkey], key, json), indent = indent))
-                except KeyError:
+                    print(dumps(reduce(lambda get, subkey: get[subkey], subkeys, json), indent = indent))
+                except:
                     pass
         else:
             print(dumps(json, indent = indent))
 
-def json_remove(file):
+def json_put(file):
+    subkeys = args.key.split(".")
+    value = args.value
+    last_index = len(subkeys) - 1
+    last_subkey = subkeys[last_index]
     for json in load(file):
-        for key in keys:
+        try:
+            put = json
+            for i in range(last_index):
+                put = put[subkeys[i]]
+            put[last_subkey] = value
+        except:
+            pass
+        print(dumps(json, indent = indent))
+
+def json_remove(file):
+    keys = list(map(lambda key: key.split("."), args.keys))
+    for json in load(file):
+        for subkeys in keys:
             try:
                 remove = json
-                last_key_index = len(key) - 1
-                for i in range(last_key_index):
-                    remove = remove[key[i]]
-                del remove[key[last_key_index]]
-            except KeyError:
+                last_index = len(subkeys) - 1
+                for i in range(last_index):
+                    remove = remove[subkeys[i]]
+                del remove[subkeys[last_index]]
+            except:
                 pass
     print(dumps(json, indent = indent))
+
+if command == "get":
+    command = json_get
+elif command == "put":
+    command = json_put
+else:
+    command = json_remove
 
 file = args.file
 if file:
     with open(file) as f:
-        (json_get if args.command == "get" else json_remove)(f)
+        command(f)
 else:
-    (json_get if args.command == "get" else json_remove)(sys.stdin)
+    command(sys.stdin)
